f5d5c93e4850cc74a7bc4ba078ce4b324c2e0314
kevin <noeamil@gmail.com>
2022-12-28 08:06:08 -0700

    added basic routes to test out app

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-""" 
+"""
 app to act as a BJJ journal
 stretch:
     add video abilites (either from storage and/or youtube link)
@@ -7,14 +7,29 @@ stretch:
     make a private page
 """
 
-from flask import Flask
+from flask import Flask, render_template
+
+from article_data import Articles
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return "hello world"
+Articles = Articles()
 
 
-if __name__ == '__main__':
-    app.run()
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/articles")
+def articles():
+    return render_template("articles.html", articles=Articles)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
